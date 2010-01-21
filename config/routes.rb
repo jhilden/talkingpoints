1750dc41de4d863004916e770734426d99223d21
jhilden <jakobhilden@gmail.com>
2010-01-21 16:30:39 -0500

    added scaffolds for: user, location_type, section, and comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :location_types
+
+  map.resources :users
+
+  map.resources :sections
+
+  map.resources :comments
+
+  map.resources :comments
+
+  map.resources :sections
+
+  map.resources :users
+
   map.resources :locations
 
   # The priority is based upon order of creation: first created -> highest priority.
