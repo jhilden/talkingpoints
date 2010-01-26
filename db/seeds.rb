edd41165fee6b2d22e422e41f4bf3669124a476a
jhilden <jakobhilden@gmail.com>
2010-01-26 12:54:01 -0500

    first version of seeds.rb.  this file creates ininitial content for the db and can be invoked with rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+location_types = LocationType.create([
+  {:name => "Restaurant"},
+  {:name => "Store"},
+  {:name => "Public building"},
+  {:name => "misc"}
+])
+coffee_shop = LocationType.create(:name => "Coffee shop")
+
+users = User.create([
+  {:username => "admin", :email => "admin@talking-points.org"},
+  {:username => "johndoe", :email => "john@doe.com"},
+  {:username => "test", :email => "test@test.com"}
+])
+jakob = User.create(:username => "jhilden", :email => "jakobhilden@gmail.com")
+
+espresso_royale = Location.create(
+  :name => "Espresso Royale", :description => "A coffee shop",
+  :location_type => coffee_shop, :user => jakob,
+  :street => "South University", :city => "Ann Arbor"
+)
