dd64acbfd465e2fd1c699763e7a36b9a0c6ae336
jhilden <jakobhilden@gmail.com>
2010-03-02 15:59:26 -0500

    missed the newly created files for the last commit

---
+++ app/controllers/info_controller.rb
@@ -0,0 +1,19 @@
+require 'rss'
+
+class InfoController < ApplicationController
+  def about
+  end
+  
+  def blog 
+    @feed = RSS::Parser.parse(open('http://talking-points.org/feed').read, false)
+  end
+  
+  def team
+  end
+  
+  def inthenews
+  end
+  
+  def contact
+  end
+end
