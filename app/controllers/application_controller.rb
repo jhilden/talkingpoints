5b926f5c90144b441debc20f354daa8d5dea3b33
jhilden <jakobhilden@gmail.com>
2010-04-19 17:27:49 -0400

    worked on the functionality to check whether the current user is an admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   
   # Scrub sensitive parameters from your log
   filter_parameter_logging :password, :password_confirmation
-  helper_method :current_user_session, :current_user
+  helper_method :current_user_session, :current_user, :current_user_is_admin?
   
   # used to make helper methods available in controllers (for location_controller.format_location_output)
   def view_helper
@@ -23,6 +23,14 @@ class ApplicationController < ActionController::Base
     def current_user
       return @current_user if defined?(@current_user)
       @current_user = current_user_session && current_user_session.user
+    end
+    
+    def current_user_is_admin?
+      if current_user
+        return @current_user.is_admin?
+      else
+        return false
+      end
     end
   
     def require_user
@@ -42,14 +50,18 @@ class ApplicationController < ActionController::Base
         return false
       end
     end
+    
     def require_admin
-      unless curent_user == "admin"
+      require_user
+      unless @current_user.is_admin?
+        logger.debug(@current_user.to_s)
         store_location
         flash[:notice] = "You must have admin privileges to do this"
-        redirect_to new_user_session_url
+        redirect_to account_url
         return false
       end
     end
+    
     def store_location
       session[:return_to] = request.request_uri
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
   has_many :comments
   
   validates_uniqueness_of :username, :email
+  
+  def is_admin?
+    return self.role == "admin"
+  end
 end
