b9ab510701635a4e33b4e0711364769d2db6d60f
jhilden <jakobhilden@gmail.com>
2010-06-07 16:14:50 -0400

    updated .gitignore to ignore sphinx configuration and added new deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,6 +21,10 @@ set :rails_env, "production"
 
 
 
+# Thinking Sphinx
+require 'thinking_sphinx/deploy/capistrano'
+
+
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -29,7 +33,9 @@ namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
-    restart_sphinx
+    thinking_sphinx.stop
+    thinking_sphinx.index
+    thinking_sphinx.start
     run "touch #{File.join(current_path,'tmp','restart.txt')}"
   end
   
@@ -37,27 +43,7 @@ namespace :deploy do
   task :symlink_database, :roles => :app do
     run "ln -s #{deploy_to}/shared/config/database.yml #{deploy_to}/current/config/database.yml"
   end
-  
-  desc "Re-establish sphinx symlinks"
-  task :symlink_sphinx do
-    run "rm -fr #{release_path}/db/sphinx && ln -nfs #{shared_path}/db/sphinx #{release_path}/db/sphinx"
-  end
-
-  desc "Stop the sphinx server"
-  task :stop_sphinx , :roles => :app do
-    run "cd #{current_path} && rake thinking_sphinx:stop RAILS_ENV=production"
-  end
-
-  desc "Start the sphinx server"
-  task :start_sphinx, :roles => :app do
-    run "cd #{current_path} && rake thinking_sphinx:configure RAILS_ENV=production && rake thinking_sphinx:index RAILS_ENV=production && rake thinking_sphinx:start RAILS_ENV=production"
-  end
-
-  desc "Restart the sphinx server"
-  task :restart_sphinx, :roles => :app do
-    stop_sphinx
-    start_sphinx
-  end
 end
 
-after 'deploy:symlink', 'deploy:symlink_database', 'deploy:symlink_sphinx'
+after 'deploy:symlink', 'deploy:symlink_database'
+after "deploy:setup", "thinking_sphinx:shared_sphinx_folder"
