d21e378c87b7477f6d31c25dc4d97f31ba3bd499
jhilden <jakobhilden@gmail.com>
2010-06-24 19:34:57 -0400

    moved the config.gem 'thinking-sphinx' line because Passenger was complaining about it and wouldn't start

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,9 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
   config.gem "geokit"
+  if RAILS_ENV == 'production'
+    config.gem "thinking-sphinx" # this is not in /config/environments/production.rb because Passenger was complaining
+  end
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,4 @@
 # Settings specified here will take precedence over those in config/environment.rb
-config.gem "thinking-sphinx"
 
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
